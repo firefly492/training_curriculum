517fbb281ca6766e6aacce6388da39c02ea88995
firefly492 <nok.tsujimura@gmail.com>
2020-07-29 15:50:24 +0900

    Revert "Issue6"
    This reverts commit 4b298b44f897d9c341b48a159cd13f6ee4a0905b.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plan = @plans.map do |plan|
         plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { month: (@todays_date + x).month, date: (@todays_date + x).day,week_end: (@week_end + x) plans: (plans)}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: (plans)}
       @week_days.push(days)
     end
   end
